04a4bcf8e84700d2b4c74fe70fa3c8914a534e9d
Hoid17 <adam.janecka@eccwireless.net>
2017-05-08 21:02:29 -0500

    Added Example 2.1

---
+++ Chapter02/Examples/2.01/Welcome1.java
@@ -0,0 +1,9 @@
+// Fig. 2.1: Welcome1.java
+//Text-printing program
+
+public class Welcome1 {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programing!");
+    } //end method main
+} // end class Welcome1
