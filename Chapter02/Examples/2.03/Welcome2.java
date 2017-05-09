98819095b8093757709f251c9fd4997c8ae364db
Hoid17 <adam.janecka@eccwireless.net>
2017-05-09 16:49:04 -0500

    Second Example from Chapter 2

---
+++ Chapter02/Examples/2.03/Welcome2.java
@@ -0,0 +1,11 @@
+//Fig. 2.3: Welcome2.java
+//Printing a line of text with multiple statements
+
+public class Welcome2 {
+
+    //main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.print("Welcome to ");
+        System.out.print("Java Programming!");
+    } // end method main
+} // end class Welcome2
