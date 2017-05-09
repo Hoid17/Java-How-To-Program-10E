1d6926b2dbd5999e849a615d2d42a88a39bb3d66
Hoid17 <adam.janecka@eccwireless.net>
2017-05-09 16:50:25 -0500

    Update Welcome2.java

--- Chapter02/Examples/2.03/Welcome2.java
+++ Chapter02/Examples/2.03/Welcome2.java
@@ -6,6 +6,6 @@ public class Welcome2 {
     //main method begins execution of Java application
     public static void main(String[] args) {
         System.out.print("Welcome to ");
-        System.out.print("Java Programming!");
+        System.out.println("Java Programming!");
     } // end method main
 } // end class Welcome2
