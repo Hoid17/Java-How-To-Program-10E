5870d7f14ecd4799c56a3a8b1a4970ad282071d2
Hoid17 <adam.janecka@eccwireless.net>
2017-05-09 16:55:04 -0500

    Third example from Chapter 2

---
+++ Chapter02/Examples/2.04/Welcome3.java
@@ -0,0 +1,9 @@
+//Fig 2.4: Welcome3.java
+// Printing multiple lines of text with a single statement.
+
+public class Welcome3 {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    } // end method main
+} // end class Welcome3
