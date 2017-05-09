ac3b5d8bb91d708a96e97b87c9045abd128734ef
Hoid17 <adam.janecka@eccwireless.net>
2017-05-09 17:00:42 -0500

    Fourth Example from Chapter 2

---
+++ Chapter02/Examples/2.06/Welcome4.java
@@ -0,0 +1,9 @@
+// Fig. 2.6: Welcome4.java
+// Displaying multiple lines with method System.out.printf.
+
+public class Welcome4 {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // end method main
+} // end class Welcome4
