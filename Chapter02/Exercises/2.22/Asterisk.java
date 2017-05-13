b1c2fda3025186db09fee194f33c0f231e0c5db1
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:26:56 -0500

    Exercise 2.22

---
+++ Chapter02/Exercises/2.22/Asterisk.java
@@ -0,0 +1,15 @@
+// Exercise 2.22
+// Find out what the code displays
+
+package pkg2_22;
+
+public class Asterisk5 {
+
+    public static void main(String[] args) {
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+    } // end method main
+} // end class Asterisk5
