e357c1eef039cc3f6a00ddeb9f6eb5624ad5f6a0
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:56:39 -0500

    Exercise 2.27

---
+++ Chapter02/Exercises/2.27/Asterisk.java
@@ -0,0 +1,19 @@
+// Create the asterisk checker pattern
+// @Author: Adam Janecka
+package pkg2_27;
+
+public class Asterisk7 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+        
+    } // end method main
+} // end class Asterisk7
