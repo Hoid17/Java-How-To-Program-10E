37cbd95540506af2f4c5818df98b599b3b573e7f
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:22:16 -0500

    Exercise 2.21

---
+++ Chapter02/Exercises/2.21/Asterisk4.java
@@ -0,0 +1,14 @@
+// Exercise 2.21
+// Find out what the code displays
+package pkg2_21;
+
+public class Asterisk4 {
+
+    public static void main(String[] args) {
+        System.out.print("*");
+        System.out.print("***");
+        System.out.print("*****");
+        System.out.print("****");
+        System.out.println("**");
+    } // end method main
+} // end class Asterisk4
