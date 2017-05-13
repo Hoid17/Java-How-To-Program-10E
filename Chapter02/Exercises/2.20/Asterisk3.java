1fb4e86f2dac567597cfa30e7da6480206cba243
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:15:13 -0500

    Exercise 2.20

---
+++ Chapter02/Exercises/2.20/Asterisk3.java
@@ -0,0 +1,14 @@
+// Exercise 2.20
+// Find out what the code displays
+package pkg2_20;
+
+public class Asterisk3 {
+    
+    public static void main(String[] args) {
+       System.out.println("*");
+       System.out.println("***");
+       System.out.println("*****");
+       System.out.println("****");
+       System.out.println("**");
+    } // end method main
+} // end class Asterisk3
