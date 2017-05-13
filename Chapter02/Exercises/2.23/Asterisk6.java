1c62e90ee41fb03abcddb466caacb4625e6d11ff
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:30:52 -0500

    Exercise 2.23

---
+++ Chapter02/Exercises/2.23/Asterisk6.java
@@ -0,0 +1,10 @@
+// Exercise 2.23
+// Find out what the code displays
+package pkg2_23;
+
+public class Asterisk6 {
+
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+    } // end method main
+} // end class Asterisk6
