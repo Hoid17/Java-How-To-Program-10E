fcb4695745038bebaca9d486a7a8e73347b04220
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 12:28:03 -0500

    Create NumberSpace.java

---
+++ Chapter02/Exercises/2.14/NumberSpace.java
@@ -0,0 +1,16 @@
+// Displays the numbers 1 through 4 
+// in three different programming methods
+package pkg2_14;
+
+public class NumberSpace {
+    public static void main(String[] args) {
+        System.out.println("1 2 3 4"); //Statement with one println statements
+        
+        System.out.print("1 "); // Statement with four print statements
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4 \n");
+        
+        System.out.printf("%s%n", "1 2 3 4"); // Statement with one printf statement
+    } // end method main
+} // end class NumberSpace
