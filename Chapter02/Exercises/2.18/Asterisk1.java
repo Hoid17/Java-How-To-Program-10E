a6f27a5de6f8aeb39d0a46fe565a0402c726d2da
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:09:12 -0500

    Exercise 2.18

---
+++ Chapter02/Exercises/2.18/Asterisk1.java
@@ -0,0 +1,17 @@
+// Display shapes with asterisks 
+// @Author: Adam Janecka
+package pkg2_18;
+
+public class Asterisk1 {
+    public static void main(String[] args) {
+        System.out.println("*********   ***     *       *");
+        System.out.println("*       *  *   *   ***     * *");
+        System.out.println("*       * *     * *****   *   *");
+        System.out.println("*       * *     *   *    *     *");
+        System.out.println("*       * *     *   *   *       *");
+        System.out.println("*       * *     *   *    *     *");
+        System.out.println("*       * *     *   *     *   *");
+        System.out.println("*       *  *   *    *      * *");
+        System.out.println("*********   ***     *       *");
+    } // end method main
+} // end class Asterisk1
