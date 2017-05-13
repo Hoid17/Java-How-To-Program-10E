355798b1c1aec59848bdede7b2d68ee1e45a3de7
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 16:29:09 -0500

    Exercise 2.29

---
+++ Chapter02/Exercises/2.29/IntegerValue.java
@@ -0,0 +1,24 @@
+// Show the ASCII representation of a few characters
+// @Author: Adam Janecka
+package pkg2_29;
+
+public class IntegerValue {
+
+    public static void main(String[] args) {
+       
+        System.out.printf("The character %c has the value %d%n", 'A', ((int) 'A'));
+        System.out.printf("The character %c has the value %d%n", 'B', ((int) 'B'));
+        System.out.printf("The character %c has the value %d%n", 'C', ((int) 'C'));
+        System.out.printf("The character %c has the value %d%n", 'a', ((int) 'a'));
+        System.out.printf("The character %c has the value %d%n", 'b', ((int) 'b'));
+        System.out.printf("The character %c has the value %d%n", 'c', ((int) 'c'));
+        System.out.printf("The character %c has the value %d%n", '0', ((int) '0'));
+        System.out.printf("The character %c has the value %d%n", '1', ((int) '1'));
+        System.out.printf("The character %c has the value %d%n", '2', ((int) '2'));
+        System.out.printf("The character %c has the value %d%n", '$', ((int) '$'));
+        System.out.printf("The character %c has the value %d%n", '*', ((int) '*'));
+        System.out.printf("The character %c has the value %d%n", '+', ((int) '+'));
+        System.out.printf("The character %c has the value %d%n", '/', ((int) '/'));
+        System.out.printf("The character %c has the value %d%n", ' ', ((int) ' '));
+    } // end method main
+} // end class IntegerValue
