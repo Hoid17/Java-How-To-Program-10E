c16b08aaade0c84e1f1c7e0bbf343120436f402c
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 16:50:39 -0500

    Exercise 2.31

---
+++ Chapter02/Exercises/2.31/SquareCube.java
@@ -0,0 +1,23 @@
+// Create a table showing the square and cubes of numbers 0-10
+// @Author: Adam Janecka
+package pkg2_31;
+
+public class SquareCube {
+
+    public static void main(String[] args) {
+        
+        System.out.println("number square cube");
+        System.out.printf("%d\t%d\t%d%n", 0, 0 * 0, 0 * 0 * 0);
+        System.out.printf("%d\t%d\t%d%n", 1, 1 * 1, 1 * 1 * 1);
+        System.out.printf("%d\t%d\t%d%n", 2, 2 * 2, 2 * 2 * 2);
+        System.out.printf("%d\t%d\t%d%n", 3, 3 * 3, 3 * 3 * 3);
+        System.out.printf("%d\t%d\t%d%n", 4, 4 * 4, 4 * 4 * 4);
+        System.out.printf("%d\t%d\t%d%n", 5, 5 * 5, 5 * 5 * 5);
+        System.out.printf("%d\t%d\t%d%n", 6, 6 * 6, 6 * 6 * 6);
+        System.out.printf("%d\t%d\t%d%n", 7, 7 * 7, 7 * 7 * 7);
+        System.out.printf("%d\t%d\t%d%n", 8, 8 * 8, 8 * 8 * 8);
+        System.out.printf("%d\t%d\t%d%n", 9, 9 * 9, 9 * 9 * 9);
+        System.out.printf("%d\t%d\t%d%n", 10, 10 * 10, 10 * 10 * 10);
+        
+    } // end method main
+} // end class SquareCube
