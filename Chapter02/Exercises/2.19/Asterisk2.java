3695aa2ea8bbf7cc67dae5816a3e33ff53037522
Hoid17 <adam.janecka@eccwireless.net>
2017-05-13 13:11:50 -0500

    Exercise 2.19

---
+++ Chapter02/Exercises/2.19/Asterisk2.java
@@ -0,0 +1,10 @@
+//Exercise 2.19
+// Find out what the code prints
+package pkg2_19;
+
+public class Asterisk2 {
+    
+    public static void main(String[] args) {
+       System.out.printf("*%n**%n***%n****%n*****%n");
+    } // end method main
+} // end class Asterisk2
