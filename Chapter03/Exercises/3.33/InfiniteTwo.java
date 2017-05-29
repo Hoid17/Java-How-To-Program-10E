a03a2ae894317cfd1be66cc383db55650cb49664
Hoid17 <adam.janecka@eccwireless.net>
2017-05-28 20:11:05 -0500

    Exercise 3.33

---
+++ Chapter03/Exercises/3.33/InfiniteTwo.java
@@ -0,0 +1,17 @@
+// Create an infinite loop showing the multiples of 2
+// @Author: Adam Janecka
+package pkg3_33;
+
+public class InfiniteTwo {
+
+    public static void main(String[] args) {
+        
+        int counter = 1; // used for while loop
+        
+        while (counter > 0)
+        {
+            System.out.printf("%d%n", 2 * counter);
+            counter++;
+        } // end while loop
+    } // end method main
+} // end class InfiniteTwo
