00c0aa0f54766e4eb0c62ce2a4914275e95427cd
Hoid17 <adam.janecka@eccwireless.net>
2017-07-15 21:04:50 -0500

    Figure 7.13

---
+++ Chapter07/Examples/7.13/DeckOfCardsTest.java
@@ -0,0 +1,23 @@
+// Fig. 7.13: DeckOfCardsTest.java
+// Card shuffling and dealing
+package pkg7_13;
+
+public class DeckOfCardsTest {
+
+    // execute application
+    public static void main(String[] args) {
+       
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle(); // place Cards in random order
+        
+        // print all 52  Cards in the order in which they are dealt
+        for (int i = 1; i <= 52; i++)
+        {
+            // deal and display a Card
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
+            
+            if (i % 4 == 0) // output a newline after every fourth card
+                System.out.println();
+        }
+    } // end method main
+} // end class DeckOfCardsTest
