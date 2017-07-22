69da2c55a9f524d6c31dea40ee475e0d0b4c0adf
Hoid17 <adam.janecka@eccwireless.net>
2017-07-21 20:13:56 -0500

    Exercise 7.13

---
+++ Chapter07/Exercises/7.13/DateTest.java
@@ -0,0 +1,14 @@
+// Application to test Date class
+// @Author: Adam Janecka
+package pkg7_13;
+
+public class DateTest {
+    
+    public static void main(String[] args)
+    {
+        // Displays the date given to constructor
+        Date today = new Date(7, 21, 2017);
+        today.displayDate();
+        System.out.println();
+    } // end method main
+} // end class DateTest
