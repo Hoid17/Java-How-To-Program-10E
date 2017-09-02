5f72683b9e1f5bb86fe4fb60b8aabef79a235244
Hoid17 <adam.janecka@eccwireless.net>
2017-09-02 11:21:26 -0500

    Create Square.java

---
+++ Chapter09/Exercises/9.08/Square.java
@@ -0,0 +1,12 @@
+// Square is a subclass of Rectangle
+// @Author: Adam Janecka
+package pkg9_08;
+
+public class Square extends Rectangle{
+    
+    public Square(double x1, double y1, double x2, double y2, 
+            double x3, double y3, double x4, double y4)
+    {
+        super(x1, y1, x2, y2, x3, y3, x4, y4);
+    } // end constructor
+} // end class Square
