a87985d5ae0e41fda2805de87e122ef7ad44fd9c
Hoid17 <adam.janecka@eccwireless.net>
2017-09-05 19:26:58 -0500

    Create Payable.java

---
+++ Chapter10/Examples/10.11/Payable.java
@@ -0,0 +1,10 @@
+// Fig. 10.11: Payable.java
+// Payable interface declaration.
+
+package pkg10_11;
+
+public interface Payable {
+
+    double getPaymentAmount(); // calculate payment; no implementation
+} // end interface Payable
+ 
