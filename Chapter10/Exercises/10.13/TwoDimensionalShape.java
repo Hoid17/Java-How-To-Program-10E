4b9ec52106f51119ef485a916b32faa4d1943adf
Hoid17 <adam.janecka@eccwireless.net>
2017-09-08 21:24:37 -0500

    Create TwoDimensionalShape.java

---
+++ Chapter10/Exercises/10.13/TwoDimensionalShape.java
@@ -0,0 +1,13 @@
+// Superclass for two-dimensional shapes
+// @Author: Adam Janecka
+package pkg10_13;
+
+public abstract class TwoDimensionalShape extends Shape{
+    
+    // toString override
+    @Override
+    public String toString()
+    {
+        return String.format("Two dimensional shape:");
+    } // end method toString
+} // end class TwoDimensionalShape
