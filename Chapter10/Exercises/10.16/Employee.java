a37b34868917b642bcfc2ea32177fc367a2f89b0
Hoid17 <adam.janecka@eccwireless.net>
2017-09-09 10:20:53 -0500

    Exercise 10.16

---
+++ Chapter10/Exercises/10.16/Employee.java
@@ -0,0 +1,53 @@
+// Modified Employee class so subclasses do not have to be modified at all
+// @Author: Adam Janecka
+package pkg10_16;
+
+public abstract class Employee implements Payable{
+
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+    
+    // constructor
+    public Employee(String firstName, String lastName, 
+            String socialSecurityNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    } // end constructor
+    
+    // return first name
+    public String getFirstName()
+    {
+        return firstName;
+    } // end method getFirstName
+    
+    public String getLastName()
+    {
+        return lastName;
+    } // end method getLastName
+    
+    // return social security number
+    public String getSocialSecurityNumber()
+    {
+        return socialSecurityNumber;
+    } // end method getSocialSecurityNumber
+    
+    // return String representation of Employee object
+    @Override
+    public String toString()
+    {
+        return String.format("%s %s%nsocial security number: %s",
+                getFirstName(), getLastName(), getSocialSecurityNumber());
+    } // end method toString
+    
+    // getPaymentAmount method
+    public double getPaymentAmount()
+    {
+       return earnings();
+    }
+    
+    public abstract double earnings(); // no implementation here
+    
+} // end abstract class Employee
