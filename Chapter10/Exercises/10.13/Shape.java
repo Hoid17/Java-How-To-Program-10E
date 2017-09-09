f95d7c3d53f589d0df779c41309b29d2241a166e
Hoid17 <adam.janecka@eccwireless.net>
2017-09-08 21:24:17 -0500

    Create Shape.java

---
+++ Chapter10/Exercises/10.13/Shape.java
@@ -0,0 +1,9 @@
+// Abstract superclass at top of hierarchy
+// @Author: Adam Janecka
+package pkg10_13;
+
+public abstract class Shape {
+   
+    // Both 2D and 3D shapes will have the getArea method
+    public abstract double getArea();
+} // end class Shape
